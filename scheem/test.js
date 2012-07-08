d0dccaf6f9ba3257dae3f3660a0072ed1c28f789
Wenshan Ren <renws1990@gmail.com>
2012-07-08 17:59:59 +1000

    Added Whitespace support, allow any number of spaces between atoms, and allow spaces around parentheses. Allow newlines and tabs as well.

--- scheem/test.js
+++ scheem/test.js
@@ -10,16 +10,18 @@ var data = fs.readFileSync("scheem.peg", "utf-8");
 // create scheem parser
 var parse = PEG.buildParser(data).parse;
 
-// basic testing
+// basic functionality test cases
 assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
 assert.deepEqual( parse("(+ a b (* (/ 10 2)))"), ["+", "a", "b", ["*", ["/", "10", "2"]]]);
 assert.deepEqual( parse("((+ 9 4))"), ['+', '9', '4'] );
 assert.deepEqual( parse("(* (+ 9 4) (+ 2 3))"), ['*', ['+', '9', '4'], ['+', '2', '3']] );
-
 assert.deepEqual(
     parse("(+ 1 (- 10 9) (* 2 1) 3 4)"),
     ["+", "1", ["-", "10", "9"], ["*", "2", "1"], "3", "4"]
 );
 
-// white space
+// white space handling test cases
 assert.deepEqual( parse(" ( a  b  c    )  "), ["a", "b", "c"]);
+assert.deepEqual( parse("(+ (- 10 9) (* 2 6) 9 7)"),
+                  ["+", ["-", "10", "9"], ["*", "2", "6"], "9", "7"]
+                );
