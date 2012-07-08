08b89f7e4fe23991e1f047f4ea921a9ab47a1f57
Wenshan Ren <renws1990@gmail.com>
2012-07-08 15:20:14 +1000

    basic scheem compiler.

---
+++ scheem/test.js
@@ -0,0 +1,25 @@
+// Unit Tests for scheem
+
+var PEG = require("pegjs");
+var assert = require("assert");
+var fs = require("fs");         // for loading files
+
+// read file contents
+var data = fs.readFileSync("scheem.peg", "utf-8");
+
+// create scheem parser
+var parse = PEG.buildParser(data).parse;
+
+// basic testing
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+assert.deepEqual( parse("(+ a b (* (/ 10 2)))"), ["+", "a", "b", ["*", ["/", "10", "2"]]]);
+assert.deepEqual( parse("((+ 9 4))"), ['+', '9', '4'] );
+assert.deepEqual( parse("(* (+ 9 4) (+ 2 3))"), ['*', ['+', '9', '4'], ['+', '2', '3']] );
+
+assert.deepEqual(
+    parse("(+ 1 (- 10 9) (* 2 1) 3 4)"),
+    ["+", "1", ["-", "10", "9"], ["*", "2", "1"], "3", "4"]
+);
+
+// white space
+assert.deepEqual( parse(" ( a  b  c    )  "), ["a", "b", "c"]);
